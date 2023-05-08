1c2d798c1b21b3fbebc98da1ccd720c48a6ebddc
rupalishinde13oct <rupalishinde13oct@gmail.com>
2023-05-08 20:38:34 +0530

    Updates

--- test_file3.py
+++ test_file3.py
@@ -1,6 +1,11 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 
 class Test3:
@@ -36,8 +41,9 @@ class Test3:
     def test_Credence(self):
         d = webdriver.Chrome()
         d.get("https://credence.in/")
-
+        time.sleep(2)
         d.find_element(By.XPATH , "//img[@src='/website/images/enquiry.png']").click()
+        time.sleep(2)
         l = len(d.find_elements(By.XPATH , "//div[@class='quickfinder-description gem-text-output']//p//a"))
         print(l)
         list = []
@@ -45,7 +51,41 @@ class Test3:
             no = d.find_element(By.XPATH , "//div[@class='quickfinder-description gem-text-output']//p//a["+str(i)+"]").text
             list.append(no)
         print(list)
-        # if "+91 9091929355" in list:
-        #     assert True
-        # else:
-        #     assert False
+        if "+91 9091929355" in list:
+            assert True
+            print("test_Credence is Passed")
+        else:
+            assert False
+            print("test_Credence is Failed")
+
+    def test_Login_001(self):
+        d = webdriver.Chrome()
+        d.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        d.maximize_window()
+
+        # will apply explicite wait on username webelement
+        wait = WebDriverWait(d, 10)
+        wait.until(ec.presence_of_element_located((By.XPATH, "//input[@placeholder='Username']")))
+        d.find_element(By.XPATH, "//input[@placeholder='Username']").send_keys("Admin")
+        d.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys("admin123")
+        d.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+
+        wait.until(ec.presence_of_element_located((By.XPATH, "//span[normalize-space()='PIM']")))
+        d.find_element(By.XPATH, "//span[normalize-space()='PIM']").click()
+        time.sleep(2)
+        d.find_element(By.XPATH, "//i[@class='oxd-icon bi-plus oxd-button-icon']").click()
+        time.sleep(2)
+        d.find_element(By.XPATH, "//input[@placeholder='First Name']").send_keys("Rupali")
+        d.find_element(By.XPATH, "//input[@placeholder='Middle Name']").send_keys("P")
+        d.find_element(By.XPATH, "//input[@placeholder='Last Name']").send_keys("Pandit")
+        d.find_element(By.XPATH, "//button[normalize-space()='Save']").click()
+        time.sleep(5)
+        # print(d.find_element(By.XPATH , "//h6[@class='oxd-text oxd-text--h6 --strong']").text())
+
+
+        # will apply explicite wait on username webelement
+        wait.until(ec.presence_of_element_located((By.XPATH, "//p[@class='oxd-userdropdown-name']")))
+        d.find_element(By.XPATH, "//p[@class='oxd-userdropdown-name']").click()
+        d.find_element(By.XPATH, "//a[normalize-space()='Logout']").click()
+
+
